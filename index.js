871c5c1615a734e0f80aed09a77deb038920b62a
Ryan Maddock <87017319+Ryan-Maddock-96@users.noreply.github.com>
2022-01-11 15:11:09 +0000

    Fixed sorting
    Fixed the sorting so you can sort the columns by ascending and descending when clicking on the table headers

--- index.js
+++ index.js
@@ -12,29 +12,36 @@ USER EDITABLE AREA
 /**************************************************************
 END
 *****************************************************************/
-const sortableTables = document.querySelectorAll('.sortable'), cellArray = [];
+const sortableTables = document.querySelectorAll('.sortable');
 if (sortableTables.length) {
     sortableTables.forEach(table => {
         const headers = Array.from(table.querySelectorAll('thead th'));
+        const rows = table.querySelectorAll('tbody tr');
         headers.forEach((header, headerIndex) => {
             header.addEventListener('click', () => {
-                const rows = table.querySelectorAll('tbody tr');
+                let filteredArray = [];
                 rows.forEach((row, index) => {
                     const cell = Array.from(row.querySelectorAll('td')).filter((e, index) => index == headerIndex).pop(), sortItem = { cellText: cell.innerHTML, rowElement: row };
-                    cellArray.push(sortItem);
+                    filteredArray.push(sortItem);
                 });
-                cellArray.sort(function (a, b) {
-                    if (isNumeric(a.cellText)) {
-                        return parseInt(a.cellText) - parseInt(b.cellText);
-                    }
-                    else {
-                        return a.cellText > b.cellText ? 1 : -1;
-                    }
-                });
+                if (!header.classList.contains('filtered')) {
+                    headers.forEach(el => el.classList.remove('filtered', 'ascending', 'descending'));
+                    header.classList.add('filtered');
+                }
+                if (!header.classList.contains('ascending')) {
+                    header.classList.remove('descending');
+                    header.classList.add('ascending');
+                    sortCells(filteredArray, rows, headerIndex, "asc");
+                }
+                else {
+                    header.classList.remove('ascending');
+                    header.classList.add('descending');
+                    sortCells(filteredArray, rows, headerIndex, "desc");
+                }
                 const tb = table.querySelector('tbody');
                 if (tb) {
                     tb.innerHTML = "";
-                    cellArray.forEach(element => {
+                    filteredArray.forEach(element => {
                         tb.append(element.rowElement);
                     });
                 }
@@ -44,4 +51,24 @@ if (sortableTables.length) {
 }
 function isNumeric(value) {
     return /^-?\d+$/.test(value);
+}
+function sortCells(cellArray, rows, headerIndex, direction) {
+    cellArray.sort(function (a, b) {
+        if (direction == "asc") {
+            if (isNumeric(a.cellText)) {
+                return Math.ceil(parseInt(a.cellText)) - Math.ceil(parseInt(b.cellText));
+            }
+            else {
+                return a.cellText > b.cellText ? 1 : -1;
+            }
+        }
+        else {
+            if (isNumeric(a.cellText)) {
+                return Math.ceil(parseInt(b.cellText)) - Math.ceil(parseInt(a.cellText));
+            }
+            else {
+                return a.cellText > b.cellText ? -1 : 1;
+            }
+        }
+    });
 }
--- index.ts
+++ index.ts
@@ -17,49 +17,74 @@ interface sortableItem {
   rowElement: HTMLTableCellElement
 }
 
-const sortableTables = document.querySelectorAll('.sortable') as NodeListOf<HTMLTableElement>,
-  cellArray = [] as  Array<sortableItem>;
+const sortableTables = document.querySelectorAll('.sortable') as NodeListOf<HTMLTableElement>;
 
 if (sortableTables.length) {
   sortableTables.forEach(table => {
     const headers = Array.from(table.querySelectorAll('thead th') as NodeListOf<HTMLTableCellElement>);
-
+    const rows = table.querySelectorAll('tbody tr') as NodeListOf<HTMLTableCellElement>;
+    
     headers.forEach((header, headerIndex) => {
+      
       header.addEventListener('click', () => {
-        const rows = table.querySelectorAll('tbody tr') as NodeListOf<HTMLTableCellElement>;
+
+        let filteredArray = [] as  Array<sortableItem>;
 
         rows.forEach((row, index) => {
           const cell = Array.from(row.querySelectorAll('td') as NodeListOf<HTMLTableCellElement>).filter((e, index) => index == headerIndex).pop() as HTMLTableCellElement,
-          sortItem: sortableItem = {cellText: cell.innerHTML, rowElement: row};
-          cellArray.push(sortItem);
-        });
-
-        cellArray.sort(function(a: sortableItem, b:sortableItem){
-          if(isNumeric(a.cellText)) {
-              return parseInt(a.cellText) - parseInt(b.cellText);
-          }else {
-            return a.cellText > b.cellText ? 1 : -1;
-          }
-
+              sortItem: sortableItem = {cellText: cell.innerHTML, rowElement: row};
+          filteredArray.push(sortItem);
         });
 
 
+        if(!header.classList.contains('filtered')){
+          headers.forEach(el => el.classList.remove('filtered', 'ascending', 'descending'));
+          header.classList.add('filtered');
+        }
+        
+        if(!header.classList.contains('ascending')) {
+          header.classList.remove('descending');
+          header.classList.add('ascending');
+          sortCells(filteredArray, rows, headerIndex, "asc");
+        } else {
+          header.classList.remove('ascending');
+          header.classList.add('descending');
+          sortCells(filteredArray, rows, headerIndex, "desc");
+        }
+        
         const tb = table.querySelector('tbody');
-if(tb){
-  tb.innerHTML = "";
-
-  cellArray.forEach(element => {
-    tb.append(element.rowElement);
-  });
-}
-
+        if(tb){
+          tb.innerHTML = "";
+        
+          filteredArray.forEach(element => {
+            tb.append(element.rowElement);
+          });
+        }
       });
     });
-
   });
 }
 
 
 function isNumeric(value:string) {
     return /^-?\d+$/.test(value);
+}
+
+function sortCells(cellArray: Array<sortableItem>, rows: NodeListOf<HTMLTableCellElement>, headerIndex: Number, direction: string) {
+    cellArray.sort(function(a: sortableItem, b:sortableItem) {
+      if(direction == "asc"){
+        if (isNumeric(a.cellText)) {
+          return parseInt(a.cellText) - parseInt(b.cellText);
+        } else {
+          return a.cellText > b.cellText ? 1 : -1;
+        }
+      } else {
+        if (isNumeric(a.cellText)) {
+          return parseInt(b.cellText) - parseInt(a.cellText);
+        } else {
+          return a.cellText > b.cellText ? -1 : 1;
+        }
+      }
+      
+    });
 }
